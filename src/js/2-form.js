c60843c61744bdfe5e78d50993724bde88f70a37
Ihor4yk <107033477+Ihor4yk@users.noreply.github.com>
2024-03-20 09:56:29 +0200

    for inspection

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,29 +1,42 @@
 const STORAGE_KEY = "feedback-form-state"
-
-const form = document.querySelector(".feedback-form");
+const form = document.querySelector('.feedback-form');
+const input = form.elements.email;
+const textarea = form.elements.message;
 
 form.addEventListener("input", addStorage);
-
-function addStorage() {
-  const jsnObject = JSON.stringify({
-    email: form.elements.email.value,
-    message: form.elements.message.value,
-  });
-  localStorage.setItem("STORAGE_KEY", jsnObject)
+function addStorage(event) {
+  const email = input.value.trim();
+  const message = textarea.value.trim();
+  const data = { email, message };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+  return data;
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  const jsonData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
-  form.elements.email.value = jsonData.email || "";
-  form.elements.message.value = jsonData.message || "";
-});
-
-form.addEventListener("submit", removeStorage)
-
-function removeStorage(event) {
+form.addEventListener("submit", handleSubmit);
+function handleSubmit(event) {
   event.preventDefault();
-  console.log(JSON.parse(localStorage.getItem("STORAGE_KEY")));
-  localStorage.removeItem("STORAGE_KEY");
-  form.elements.email.value = "";
-  form.elements.message.value = "";
+  const emailValue = input.value.trim();
+  const messageValue = textarea.value.trim();
+  if (emailValue === "" || messageValue === "") {
+    console.log("No saved data");
+  } else {
+    const saveData = addStorage();
+    console.log(saveData);
+    localStorage.removeItem(STORAGE_KEY);
+    form.reset();
+  }
 }
+
+function pageRefresh() {
+  const json = localStorage.getItem(STORAGE_KEY) ?? "";
+  try {
+    const data = JSON.parse(json);
+    console.log(data);
+    input.value = data.email;
+    textarea.value = data.message;
+  }
+  catch {
+    console.log("No saved data");
+  }
+}
+pageRefresh();
